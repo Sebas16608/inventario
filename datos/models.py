9b6129c4d9f18b677205b22d10da36ccf0b8f8cf
Sebas16608 <asebasrr444@gmail.com>
2025-11-05 23:51:37 -0600

    Arreglando y terminando los primeros modelos de prueba

--- datos/models.py
+++ datos/models.py
@@ -1,24 +1,50 @@
 from django.db import models
 
+# Agregar categoria
+# restar o sumar cada que se agrega o se retira producto
+# Costo de producto grande en pocas cantidades ejemplo Litros a ML
 # Create your models here.
 class Category(models.Model):
-    nombre = models.CharField(max_lenght=255)
-    molecula = models.CharField(max_lenght=255)
+    nombre = models.CharField(max_length=255)
+    moleculas = models.CharField(max_length=255, blank=True, null=True)
+    especie = models.CharField(max_length=255, blank=True, null=True)
+    etapa = models.CharField(max_length=255, blank=True, null=True)
+    presentacion = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = "Categoria"
         verbose_name_plural = "Categorias"
 
     def __str__(self):
-        return self.name
+        return self.nombre
 
-class Dato(models.Model):
-    categoria = models.ForeInKey(Category, on_delete=models.PROTECT, related_name="dato")
-    nombre = modesl.CharField(max_lenght=255)
-    introduccion = models.Charfield(max_lenght=255)
-    slug = models.SlugField(max_lenght=255, unique=True)
-    descripcion = models.TextField()
-    cantidad = models.IntegredField()
-    precio = models.DecimalField(max_digits=20, decimal_places=2)
-    descuento = models.FloatField()
 
+class Datos(models.Model):
+    categoria = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="datos")
+    nombre = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+    presentacion = models.CharField(max_length=255, blank=False, null=False)
+    fecha_fabricacion = models.DateField()
+    fecha_caducidad = models.DateField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_descuento = models.FloatField(blank=True, null=True)
+ 
+    class Meta:
+        verbose_name = "Dato"
+        verbose_name_plural = "Datos"
+
+    def __str__(self):
+        return f"De {self.nombre} hay {self.cantidad} unidades"
+
+    def pocacantidad(self):
+        if self.cantidad <= 5:
+            return f"Se necesita mas producto solo hay {self.cantidad} unidad/es"
+        return f"Stock suficiente hay {self.cantidad} unidades"
+   
+    def precio_total(self):
+        if self.precio_descuento is None:
+            return f"Sin descuento aplicado: {self.precio}"
+    
+        precio_final = self.precio + self.precio_descuento
+        return f"Con descuento aplicado: {precio_final:.2f}"
+  
