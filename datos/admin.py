2f44201d0a6b36e8cdc576a65119bfcfd90ee18a
Sebas16608 <asebasrr444@gmail.com>
2025-11-10 22:27:01 -0600

    Personalizando en admin

--- datos/admin.py
+++ datos/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Category, Datos
+from .models import Category, Datos, SacarDatos
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Datos)
+@admin.register(Datos)
+class DatosAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "cantidad", "categoria", "presentacion", "pocacantidad", "precio", "precio_descuento", "precio_total"]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "moleculas", "especie", "etapa", "presentacion"]
+
+@admin.register(SacarDatos)
+class SacarDatosAdmin(admin.ModelAdmin):
+    list_display = ["datos", "cantidad_sacada", "fecha_salida"]
