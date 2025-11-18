7601072143e1dadccf17911ab6e2d146702a59de
Sebas16608 <asebasrr444@gmail.com>
2025-11-17 22:08:47 -0600

    Definiendo el metodo POST de la API de categorias de MALLO

--- core/views/mallo.py
+++ core/views/mallo.py
@@ -23,4 +23,11 @@ class MalloCategoryView(APIView):
             serializer = MalloCategorySerializer(category, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         
-        
+    def post(self, request):
+        serializer = MalloCategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
