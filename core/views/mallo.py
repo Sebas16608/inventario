cc112d5d49cc119cf0fdb32daeb089a19348527f
Sebas16608 <asebasrr444@gmail.com>
2025-11-21 11:13:49 -0600

    Definiendo el metodo POST de la API de datos de mallo

--- core/views/mallo.py
+++ core/views/mallo.py
@@ -66,4 +66,10 @@ class MalloDatosView(APIView):
             dato = MalloDatos.objects.all()
             serializer = MalloDatosSerializer(dato, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-        
+    
+    def post(self, request):
+        serializer = MalloDatosSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
