f912adde29eb342686fad3883353d8f36cdd3f2d
Sebas16608 <asebasrr444@gmail.com>
2025-11-21 20:36:36 -0600

    Preparando el Admin para probar los modelos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from models.mallo.py import MalloCategory, MalloDatos, MalloSacarDatos
+from models.empresa.py import Empresa, PerfilUsuario
 # Register your models here.
+
+# MALLO
+admin.site.register(MalloCategory)
+admin.site.register(MalloDatos)
+admin.site.register(MalloSacarDatos)
+
+# Empresa
