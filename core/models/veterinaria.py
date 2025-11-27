8a165fd28495ee42e408d9ac16f30087353112ce
Sebas16608 <asebasrr444@gmail.com>
2025-11-27 06:52:58 -0600

    Empezando a definir los modelos con los campos correspondientes que el cliente pidio

--- core/models/veterinaria.py
+++ core/models/veterinaria.py
@@ -1,7 +1,9 @@
 from django.db import models
+from core.models.empresa import Empresa
 
 class VetCategory(models.Model):
     nombre = models.CharField(max_length=255)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, related_name="categorias")
 
     class Meta:
         verbose_name = "Categoria"
@@ -15,8 +17,17 @@ class VetDatos(models.Model):
     nombre = models.CharField(max_length=255)
     cantidad = models.IntegerField()
     presentacion = models.CharField(max_length=255)
-    fecha_fabricacion = models.DateField()
     fecha_caducidad = models.DateField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
+    class Meta:
+        verbose_name = "Dato Veterinario"
+        verbose_name_plural = "Datos Veterinarios"
+
+    def __str__(self):
+        return self.nombre
     
+class VetSacarDatos(models.Model):
+    datos = models.ForeignKey(VetDatos, on_delete=models.PROTECT, related_name="salidas")
+    cantidad_sacada = None
