3daab402e5276a414b100fa7c486c69b7f1e884f
Sebas16608 <asebasrr444@gmail.com>
2025-12-01 00:19:38 -0600

    Definiendo el metodo PUT de la API de entradas de mallo

--- core/views/mallo.py
+++ core/views/mallo.py
@@ -159,3 +159,16 @@ class MalloEntradaView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        try:
+            entrada = MalloEntrada.objects.get(pk=pk)
+        except MalloEntrada.DoesNotExist:
+            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = MalloEntradaSerializer(entrada, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
