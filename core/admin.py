1beea8703c898573b0ad380c4003499d11bae041
Sebas16608 <asebasrr444@gmail.com>
2025-12-05 22:36:41 -0600

    Registrando los modelos de veterinaria en el admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models.mallo import MalloCategory, MalloDatos, MalloSacarDatos, MalloEntrada
 from .models.empresa import Empresa, PerfilUsuario
+from .models.veterinaria import VetCategory, VetDatos, VetSacarDatos
 # Register your models here.
 
 # MALLO
@@ -8,6 +9,11 @@ admin.site.register(MalloCategory)
 admin.site.register(MalloDatos)
 admin.site.register(MalloSacarDatos)
 admin.site.register(MalloEntrada)
+
+# Veterinaria
+admin.site.register(VetCategory)
+admin.site.register(VetDatos)
+admin.site.register(VetSacarDatos)
 
 # Empresa
 admin.site.register(Empresa)
