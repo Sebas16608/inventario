00f08970d60b74d8862d2d5845ce8add6a62c352
Sebas16608 <asebasrr444@gmail.com>
2025-12-06 23:18:28 -0600

    inicializando la API y tambien definiendo el metodo GET de la API de categorias

---
+++ core/views/veterinaria.py
@@ -0,0 +1,23 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from core.models.veterinaria import VetCategory, VetDatos, VetEntrada, VetSacarDatos
+from core.serializers.veterinaria import VetCategorySerializer, VetDatosSerializer, VetEntradaSerializer
+from core.permissions.veterinaria import VetPermisosEscritura, EsSuperUser, VetPermisosLectura, VetPermisos
+
+def notexist():
+    return {"error": "los datos no fueron encontrados"}
+
+class VetCategoryView(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                category = VetCategory.objects.get(pk=pk)
+                serializer = VetCategorySerializer(category)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except VetCategory.DoesNotExist:
+                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+        else:
+            category = VetCategory.objects.all()
+            serializer = VetCategorySerializer(category, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
