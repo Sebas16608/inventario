37b643c197db05e2d9c087cf100724670fbd6303
Sebas16608 <asebasrr444@gmail.com>
2025-12-08 20:06:13 -0600

    Definiendo el metodo PUT de la API de datos de la veterinaria

--- core/views/veterinaria.py
+++ core/views/veterinaria.py
@@ -69,4 +69,17 @@ class VetDatosView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def put(self, request, pk):
+        try:
+            datos = VetDatos.objects.get(pk=pk)
+        except VetDatos.DoesNotExist:
+            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
         
+        serializer = VetDatosSerializer(datos, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
