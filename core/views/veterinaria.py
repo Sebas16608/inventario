4c3aaf6d7b36a499ef202bad91567e7c1179a078
Sebas16608 <asebasrr444@gmail.com>
2025-12-11 22:29:58 -0600

    Creando las Nuevas APIs solo con la API que creamos por aparte

--- core/views/veterinaria.py
+++ core/views/veterinaria.py
@@ -2,136 +2,26 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from core.models.veterinaria import VetCategory, VetDatos, VetEntrada, VetSacarDatos
+from core.views.API import SuperApiView
 from core.serializers.veterinaria import VetCategorySerializer, VetDatosSerializer, VetEntradaSerializer, VetSalidaSerializer
 from core.permissions.veterinaria import VetPermisosEscritura, EsSuperUser, VetPermisosLectura, VetPermisos
 
-def notexist():
-    return {"error": "los datos no fueron encontrados"}
+class VetCategoryView(SuperApiView):
+    permission_classes = []
+    model  = VetCategory
+    serializer_class = VetCategorySerializer
 
-class VetCategoryView(APIView):
-    def get(self, request, pk=None):
-        if pk:
-            try:
-                category = VetCategory.objects.get(pk=pk)
-                serializer = VetCategorySerializer(category)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except VetCategory.DoesNotExist:
-                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        else:
-            category = VetCategory.objects.all()
-            serializer = VetCategorySerializer(category, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-    def post(self, request):
-        serializer = VetCategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self, request, pk):
-        try:
-            category = VetCategory.objects.get(pk=pk)
-        except VetCategory.DoesNotExist:
-            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = VetCategorySerializer(category, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.errors)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        try:
-            category = VetCategory.objects.get(pk=pk)
-        except VetCategory.DoesNotExist:
-            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class VetDatosView(SuperApiView):
+    permission_classes = []
+    model = VetDatos
+    serializer_class = VetDatosSerializer
 
-class VetDatosView(APIView):
-    def get(self, request, pk=None):
-        if pk:
-            try:
-                datos = VetDatos.objects.get(pk=pk)
-                serializer = VetDatosSerializer(datos)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except VetDatos.DoesNotExist:
-                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        else:
-            datos = VetDatos.objects.all()
-            serializer = VetDatosSerializer(datos, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = VetDatosSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+class VetEntradaView(SuperApiView):
+    permission_classes = []
+    model = VetEntrada
+    serializer_class = VetEntradaSerializer
 
-    def put(self, request, pk):
-        try:
-            datos = VetDatos.objects.get(pk=pk)
-        except VetDatos.DoesNotExist:
-            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = VetDatosSerializer(datos, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        try:
-            datos = VetDatos.objects.get(pk=pk)
-        except VetDatos.DoesNotExist:
-            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        
-        datos.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class VetEntradaView(APIView):
-    def get(self, request, pk=None):
-        if pk:
-            try:
-                entrada = VetEntrada.objects.get(pk=pk)
-                serializer = VetEntradaSerializer(entrada)
-                return Response(serializer.dada, status=status.HTTP_200_OK)
-            except VetEntrada.DoesNotExist:
-                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        else:
-            entrada = VetEntrada.objects.all()
-            serializer = VetEntradaSerializer(entrada, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = VetEntradaSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def put(self, request, pk):
-        try:
-            entrada = VetEntrada.objects.get(pk=pk)
-        except VetEntrada.DoesNotExist:
-            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = VetEntradaSerializer(entrada, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    def delete(self, request, pk):
-        try:
-            entrada = VetEntrada.objects.get(pk=pk)
-        except VetEntrada.DoesNotExist:
-            return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
-
-        entrada.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-        
+class VetSalidaView(SuperApiView):
+    permission_classes = []
+    model = VetSacarDatos
+    serializer_class = VetSalidaSerializer
